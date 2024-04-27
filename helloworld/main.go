efd15997d312741bf1e746563f3b40c8036b76ec
linqi <wkw4399xs@gmail.com>
2024-04-28 04:11:47 +0800

    gin 框架入门:获取普通参数

--- helloworld/main.go
+++ helloworld/main.go
@@ -5,6 +5,12 @@ import (
 	"log"
 )
 
+type User struct {
+	Id      int64  `form:"id"`
+	Name    string `form:"name"`
+	Address string `form:"address"`
+}
+
 func main() {
 	r := gin.Default()
 	// gin 框架分组路由测试
@@ -31,6 +37,30 @@ func main() {
 			})
 		})
 	}
+	// 获取普通参数,获取方法 1，直接获取
+	r.GET("/user/save", func(ctx *gin.Context) {
+		id := ctx.Query("id")
+		name := ctx.Query("name")
+		// 地址不存在，给一个默认值
+		address := ctx.DefaultQuery("address", "beijing")
+		ctx.JSON(200, gin.H{
+			"code":    200,
+			"id":      id,
+			"name":    name,
+			"address": address,
+		})
+	})
+
+	// 获取普通参数,获取方法 2 ，对象获取
+	r.GET("/user/test", func(ctx *gin.Context) {
+		var user User
+		// BindQuery 返回的是一个列表参数
+		err := ctx.ShouldBind(&user)
+		if err != nil {
+			log.Println(err)
+		}
+		ctx.JSON(200, user)
+	})
 
 	err := r.Run(":9090")
 	if err != nil {
